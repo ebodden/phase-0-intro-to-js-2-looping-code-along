f88797fb8861f5a9d8df6cf4b66dd92869fc736f
ebony <info@ebonysontv.com>
2024-08-06 22:23:14 -0400

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards( name, event) {
+  let array =[];
+  for (let i = 0; i < name.length; i++) {
+    let message = (`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    array.push(message)
+  }
+  return array;
+}
+let cards = writeCards(["Guadalupe", "Ollie","Aki"],"birthday",);
+console.log(cards)
+
+
+function countDown(start) {
+let i = start; 
+while (i >= 0) {
+  console.log(i);
+  i--;
+ }
+}
+countDown(10);
